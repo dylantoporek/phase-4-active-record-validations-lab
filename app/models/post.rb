9f3471d1d4039cee0803eae3cb7e3b02b087e15e
DylanToporek <dylantoporek@gmail.com>
2022-01-15 15:07:10 -0500

    commit tree c4d903db8d98cbd2b079478ecd56f9d01fdf296d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    validate :title_check
+
+    def title_check
+        unless "#{title}".include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+            errors.add(:title, "Not clickbaity enough!")
+        end
+    end
 end
